984449b9027a2a5f8197ad02d0b26a83eaf29f51
Shaswat Prabhat <spd1238@gmail.com>
2021-04-02 15:42:41 +0530

    Explore pretty printing and print debug with Rust.

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -7,9 +7,7 @@
 
 // This is the main function
 fn main() {
-    // Statements here are executed when the compiled binary is called
-
-    // Print text to the console
-    println!("Hello World!");
-    println!("I'm a Rustacean!!");
+    print!("This is a print command without a new line");
+    println!("This is a println commend so it will add a new line to its end");
+    println!("New line here")
 }
