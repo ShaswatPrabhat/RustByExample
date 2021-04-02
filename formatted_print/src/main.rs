d512eb49feeab84adb4846b871cf81d78de85b16
Shaswat Prabhat <spd1238@gmail.com>
2021-04-02 15:26:37 +0530

    Added decimal digits of precision handling.

--- formatted_print/src/main.rs
+++ formatted_print/src/main.rs
@@ -1,11 +1,21 @@
 fn main() {
+    let pi = 3.141592;
     // In general, the `{}` will be automatically replaced with any
     // arguments. These will be stringified.
     println!("{} days", 31);
     println!("{0} this is {1}. {1}, this is {0}", "Bubu", "Zuzu");
-    println!("{subject} {verb} {object}",
-             object="the lazy dog",
-             subject="the quick brown fox",
-             verb="jumps over");
-    println!("{} of {:b} people know binary, the other half doesn't", 1, 2);
+    println!(
+        "{subject} {verb} {object}",
+        object = "the lazy dog",
+        subject = "the quick brown fox",
+        verb = "jumps over"
+    );
+    println!(
+        "{} of {:b} people know binary, the other half doesn't",
+        1, 2
+    );
+    // Attempt to right align printed output
+    println!("{number:>0width$}", number = 420, width = 10);
+    // Attempt to control number of decimal digits on pi
+    println!("Value of {} is {:.3}", "pi", pi);
 }
